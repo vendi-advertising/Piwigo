f8638bf97f0b6631cbc33db8d47f622718de9ce9
Matthieu Leproux <leproux.matthieu@gmail.com>
2021-10-06 16:42:10 +0200

    related to #1465 better indent, new translation line + add guest as filter

--- language/fr_FR/common.lang.php
+++ language/fr_FR/common.lang.php
@@ -416,3 +416,4 @@ $lang['generate random password'] = 'générer un mot de passe aléatoire';
 $lang['Related albums'] = 'Albums liés';
 $lang['Album name, 1 &rarr; 5 &rarr; 10 &rarr; 100'] = 'Nom d\'album, 1 &rarr; 5 &rarr; 10 &rarr; 100';
 $lang['Album name, 100 &rarr; 10 &rarr; 5 &rarr; 1'] = 'Nom d\'album, 100 &rarr; 10 &rarr; 5 &rarr; 1';
+$lang['Start-Date'] = 'Date de début';
